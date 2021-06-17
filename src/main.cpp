ebef0f74d92953ef253337ba553540639806fa12
Appenzeill <daanroth@protonmail.com>
2021-06-17 10:58:17 +0200

    Library werkt correct nu, structuur aanpassen zodat deze makkelijker kan worden gebruikt voor main project.

--- src/main.cpp
+++ src/main.cpp
@@ -14,35 +14,34 @@ int main( void ){
   
   auto oled    = hwlib::glcd_oled( i2c_bus, 0x3c ); 
   auto mpu6050_a = mpu6050_diy(i2c_bus, 0x68);
-  // auto mpu6050_b = mpu6050_diy(i2c_bus, 0x69);
+  auto mpu6050_b = mpu6050_diy(i2c_bus, 0x69);
 
   auto font    = hwlib::font_default_8x8();
   auto display = hwlib::terminal_from( oled, font );
   
   mpu6050_a.wakeup();
-  // mpu6050_b.wakeup();
+  mpu6050_b.wakeup();
   
 
   for(;;) {
-    int16_t x_complete_a = mpu6050_a.getGyroX();
-    // int16_t y_complete_a = mpu6050_a.getGyroY();
-    // int16_t z_complete_a = mpu6050_a.getGyroZ();
+    int16_t x_complete_a = mpu6050_a.getAccelX();
+    int16_t y_complete_a = mpu6050_a.getAccelY();
+    int16_t z_complete_a = mpu6050_a.getAccelZ();
 
-    // int16_t x_complete_b = mpu6050_b.getGyroX();
-    // int16_t y_complete_b = mpu6050_b.getGyroY();
-    // int16_t z_complete_b = mpu6050_b.getGyroZ();
+    int16_t x_complete_b = mpu6050_b.getAccelX();
+    int16_t y_complete_b = mpu6050_b.getAccelY();
+    int16_t z_complete_b = mpu6050_b.getAccelZ();
 
+    hwlib::cout << "test";
     display 
       << "\f" << "x: " << x_complete_a
-      // << "\n" << "y: " << y_complete_a
-      // << "\t0002" << "z: " << z_complete_a
+      << "\n" << "y: " << y_complete_a
+      << "\t0002" << "z: " << z_complete_a
 
-      // << "\t0004" << "x: " << x_complete_b
-      // << "\t0005" << "y: " << y_complete_b
-      // << "\t0006" << "z: " << z_complete_b
-
-
-
+      << "\t0004" << "x: " << x_complete_b
+      << "\t0005" << "y: " << y_complete_b
+      << "\t0006" << "z: " << z_complete_b
+      
       << hwlib::flush;
   }  
 }
--- src/mpu6050_diy.hpp
+++ src/mpu6050_diy.hpp
@@ -1,26 +1,27 @@
-
-
 #include <vector>
+#include <string>
 #include <math.h>
 class mpu6050_diy {
 private:
   hwlib::i2c_bus & i2c_bus;
   uint8_t i2c_address;
 
-  // uint8_t gyro_x_first_half, gyro_x_second_half;
   // registers
   uint8_t wake_up  = 0x6B;
 
-  uint8_t acel_x_h = 0x3B;
-  uint8_t acel_x_l = 0x3C;
+  uint8_t accel_x_h = 0x3B;
+  uint8_t accel_x_l = 0x3C;
+  uint8_t accel_y_h = 0x3D;
+  uint8_t accel_y_l = 0x3E;
+  uint8_t accel_z_h = 0x3F;
+  uint8_t accel_z_l = 0x40;
 
-  
-  uint8_t gyro_x_h = 0x43;
-  uint8_t gyro_x_l = 0x44;
-  uint8_t gyro_y_h = 0x45;
-  uint8_t gyro_y_l = 0x46;
-  uint8_t gyro_z_h = 0x47;
-  uint8_t gyro_z_l = 0x48;
+  uint8_t gyro_x_h    = 0x43;
+  uint8_t gyro_x_l    = 0x44;
+  uint8_t gyro_y_h    = 0x45;
+  uint8_t gyro_y_l    = 0x46;
+  uint8_t gyro_z_h    = 0x47;
+  uint8_t gyro_z_l    = 0x48;
 
 public:
   // Constructor van de class.
@@ -36,24 +37,117 @@ public:
     i2c_bus.write(i2c_address).write(data, byte_size);
   }
 
-  int16_t getGyroX() {
+  int16_t getAccelX() {
     uint8_t first_half, second_half;
+    int16_t result;
+    
+    const uint8_t data_0[] = {accel_x_h};
+    const uint8_t data_1[] = {accel_x_l};
 
-    uint8_t result;
-    const uint8_t data_0[] = {gyro_x_h};
-    size_t byte_size_0 = sizeof(data_0);
-
-    const uint8_t data_1[] = {gyro_x_l};
-    size_t byte_size_1 = sizeof(data_1);
-
-    i2c_bus.write(i2c_address).write(data_0, byte_size_0);
+    i2c_bus.write(i2c_address).write(data_0, sizeof(data_0));
     i2c_bus.read(i2c_address).read(first_half);
 
-    i2c_bus.write(i2c_address).write(data_1, byte_size_1);
+    i2c_bus.write(i2c_address).write(data_1, sizeof(data_1));
     i2c_bus.read(i2c_address).read(second_half);
 
     result = (first_half << 8) + second_half;
+    result = round(result / 65.4); 
+
     return result;
   }
 
- };
+  int16_t getAccelY() {
+    uint8_t first_half, second_half;
+    int16_t result;
+    
+    const uint8_t data_0[] = {accel_y_h};
+    const uint8_t data_1[] = {accel_y_l};
+
+    i2c_bus.write(i2c_address).write(data_0, sizeof(data_0));
+    i2c_bus.read(i2c_address).read(first_half);
+
+    i2c_bus.write(i2c_address).write(data_1, sizeof(data_1));
+    i2c_bus.read(i2c_address).read(second_half);
+
+    result = (first_half << 8) + second_half;
+    result = round(result / 65.4); 
+
+    return result;
+  }
+
+    int16_t getAccelZ() {
+    uint8_t first_half, second_half;
+    int16_t result;
+    
+    const uint8_t data_0[] = {accel_z_h};
+    const uint8_t data_1[] = {accel_z_l};
+
+    i2c_bus.write(i2c_address).write(data_0, sizeof(data_0));
+    i2c_bus.read(i2c_address).read(first_half);
+
+    i2c_bus.write(i2c_address).write(data_1, sizeof(data_1));
+    i2c_bus.read(i2c_address).read(second_half);
+
+    result = (first_half << 8) + second_half;
+    result = round(result / 65.4) / 1; 
+
+    return result;
+  }
+
+  int16_t getGyroX() {
+    uint8_t first_half, second_half;
+    int16_t result;
+    
+    const uint8_t data_0[] = {gyro_x_h};
+    const uint8_t data_1[] = {gyro_x_l};
+    
+    i2c_bus.write(i2c_address).write(data_0, sizeof(data_0));
+    i2c_bus.read(i2c_address).read(first_half);
+    
+    i2c_bus.write(i2c_address).write(data_1, sizeof(data_1));
+    i2c_bus.read(i2c_address).read(second_half);
+    
+    result = (first_half << 8) + second_half;
+    result = round(result / 65.4); 
+
+    return result;
+  }
+
+  int16_t getGyroY() {
+    uint8_t first_half, second_half;
+    int16_t result;
+    
+    const uint8_t data_0[] = {gyro_y_h};
+    const uint8_t data_1[] = {gyro_y_l};
+    
+    i2c_bus.write(i2c_address).write(data_0, sizeof(data_0));
+    i2c_bus.read(i2c_address).read(first_half);
+    
+    i2c_bus.write(i2c_address).write(data_1, sizeof(data_1));
+    i2c_bus.read(i2c_address).read(second_half);
+    
+    result = (first_half << 8) + second_half;
+    result = round(result / 65.4); 
+
+    return result;
+  }
+
+   int16_t getGyroZ() {
+    uint8_t first_half, second_half;
+    int16_t result;
+    
+    uint8_t data_0[] = {gyro_z_h};
+    uint8_t data_1[] = {gyro_z_l};
+    
+    i2c_bus.write(i2c_address).write(data_0, sizeof(data_0));
+    i2c_bus.read(i2c_address).read(first_half);
+    
+    i2c_bus.write(i2c_address).write(data_1, sizeof(data_1));
+    i2c_bus.read(i2c_address).read(second_half);
+    
+    result = (first_half << 8) + second_half;
+    result = round(result / 65.4); 
+
+    return result;
+  }
+};
